49fe0ac90eb6ef6d1117c0b45447227439b76d41
naresh sharma <naresh.sharma.connexials@gmail.com>
2022-11-17 11:43:58 +0530

    changes status in session controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,7 +4,7 @@ class Users::SessionsController < ApplicationController
   def create
     @user = User.find_by!(email: session_params.fetch(:email))
     unless @user.authenticate(session_params.fetch(:password))
-      render json: {msg: 'email/password is wrong'}, status: :ok and return
+      render json: {msg: 'email/password is wrong'}, :status => :unauthorized and return
     end
     
     render json: {msg: 'success',user_id: @user.id, role: @user.role, token: encode({user_id: @user.id, email: @user.email, first_name: @user.first_name})}
