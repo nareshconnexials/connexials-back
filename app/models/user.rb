b86cafdfea11600a335ef490f18db93e49f9ee48
naresh sharma <naresh.sharma.connexials@gmail.com>
2022-11-28 13:17:16 +0530

    importing models

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  has_secure_password
+  enum role: [:employee, :admin]
+
+  ## Assocation
+  has_many :leaves
+
+  ## Callback
+  after_initialize do
+    if self.new_record?
+      self.role ||= :employee
+    end
+  end
+end
