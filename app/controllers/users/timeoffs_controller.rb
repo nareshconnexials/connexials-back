8ea17dea76e8148cf052a41d0cabbd6313bc3629
naresh sharma <naresh.sharma.connexials@gmail.com>
2022-11-28 17:53:05 +0530

    rename the controller of leaves (timeoffs)

---
+++ app/controllers/users/timeoffs_controller.rb
@@ -0,0 +1,29 @@
+class Users::timeoffsController < ApplicationController
+
+  def index
+    @timeoff = Timeoff.all
+    
+    render json: @timeoff
+  end
+
+  def create
+    @timeoff = Timeoff.create! timeoff_params
+    if @timeoff.persisted?
+      render json: { message: "Timeoff created successfully", data: @timeoff}, status: :created and return
+    end
+    render json: { message: "Timeoff could not be created successfully", errors: @timeoff.errors.full_messages }, status: :unprocessable_entity
+  
+  end
+
+  def show
+    @timeoff= Timeoff.find_by_id!(params[:id])
+    
+    render json: @timeoff
+  end
+
+  private
+
+  def timeoff_params
+    params.require(:timeoff).permit(:user_id, :from_date, :to_date, :from_session, :to_session, :days, :mail_to, :reason)
+  end
+end
