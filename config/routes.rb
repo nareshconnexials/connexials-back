0590e3d3549ac52f56c60cb85f60220af8790a69
naresh sharma <naresh.sharma.connexials@gmail.com>
2022-11-28 13:58:00 +0530

    Deduction dashboard added

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     resources :users
     resources :allowances
     # resources :employments
-    # resources :deductions
+    resources :deductions
     # resources :payslips
     # resources :leaves
     root to: "users#index"
