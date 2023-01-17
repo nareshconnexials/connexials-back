a256abbebfca1ab02f55c313cce0eb07c0386b03
nareshconnexials <naresh.sharma.connexials@gmail.com>
2023-01-17 13:28:13 +0530

    Authenticate user and hot fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   include Authenticable
 
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
 
   protect_from_forgery with: :null_session,
   if: Proc.new { |c| c.request.format =~ %r{application/json} }
